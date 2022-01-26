aa8240080d1b6929259cf6d28d281d1b83b3e489
PrakharGupta36 <prakhargupta3680@gmail.com>
2022-01-26 19:07:54 +0530

    Almost done

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,60 @@
-let radioButton = document.getElementsByClassName("radio-button");
+function theme() {
+  let radioButton = document.querySelectorAll(".radio-button");
 
-for (let i = 0; i < radioButton.length; i++) {
-  setInterval(() => {
-    if (radioButton[i].checked === true) {
-      document.body.className = radioButton[i].value;
-    }
-  }, 500);
+  radioButton.forEach((e) => {
+    setInterval(() => {
+      if (e.checked === true) {
+        document.body.className = e.value;
+      }
+    });
+  });
 }
 
+theme();
 
+function main() {
+  let input = document.getElementById("input");
+  let numButtons = document.querySelectorAll(".num-button");
+  let deleteButton = document.getElementById("delete-button");
+  let opertors = document.querySelectorAll(".opertors");
+  let equal = document.getElementById("equal");
+  let reset = document.getElementById("reset");
+
+  numButtons.forEach((e) => {
+    e.addEventListener("click", () => {
+      input.value += e.value;
+    });
+  });
+
+  deleteButton.onclick = () => {
+    let inputValue = input.value.slice(0, -1);
+    // inputValue = inputValue.slice(0, -1);
+    input.value = inputValue;
+  };
+
+  reset.onclick = () => {
+    input.value = "";
+  };
+
+  opertors.forEach((e) => {
+    e.addEventListener("click", () => {
+      let lastchar = input.value[input.value.length - 1];
+      if (
+        input.value.length > 0 &&
+        (lastchar !== "+" ||
+          lastchar !== "-" ||
+          lastchar !== "*" ||
+          lastchar !== "/")
+      ) {
+        input.value += e.value;
+      }
+    });
+  });
+
+  equal.onclick = () => {
+    input.value = eval(input.value);
+    console.log(input.value[input.value.length - 1]);
+  };
+}
+
+main();
